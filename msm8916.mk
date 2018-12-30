57fd55dbf3f211577c5453f3db1eb6524b90e726
DroidFreak32 <rushabshah32@gmail.com>
2018-12-30 10:59:18 +0530

    Revert "camera: Specify internal video devices to ignore by the external provider" Fixes camera. 12-30 10:43:52.421   480  4048 E mm-camera-intf: mm_camera_open: dev name = /dev/, cam_idx = 0 12-30 10:43:52.421   480  4048 E mm-camera-intf: mm_camera_open:  opened, break out while loop 12-30 10:43:52.421   480  4048 E mm-camera-intf: mm_camera_open: cannot open control fd of '/dev/' (Is a directory) 12-30 10:43:52.421   480  4048 E mm-camera-intf: camera_open_rc: mm_camera_open err = -1 12-30 10:43:52.421   480  4048 E QCamera2HWI: camera_open failed with return code -2147483648 12-30 10:43:52.423   480  4048 E CameraWrapper: vendor camera open fail 12-30 10:43:52.423   480  4048 E CamDev@1.0-impl: Could not open camera 0: -19 12-30 10:43:52.423   480  4048 E CameraHardwareInterface: initialize: openSession failed! Function not implemented (-38) 12-30 10:43:52.423   480  4048 E CameraClient: initialize: Camera 0: unable to initialize device: Function not implemented (-38) 12-30 10:43:52.423   480  4048 I CameraHardwareInterface: Destroying camera 0 12-30 10:43:52.423   480  4048 E CameraService: connectHelper: Could not initialize client from HAL. This reverts commit 767778b02ea4e48d3dbfebbf1c0f448b87c6484a.
    Change-Id: I6051d919c8b3a17bf3512a5280061a3dbb708a24

--- msm8916.mk
+++ msm8916.mk
@@ -80,9 +80,6 @@ PRODUCT_COPY_FILES +=  \
     $(LOCAL_PATH)/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf
 
 # Camera
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
-
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
